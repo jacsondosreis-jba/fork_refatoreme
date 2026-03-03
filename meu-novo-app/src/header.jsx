2be418167acfd6a659cfe3b693192f9092a10491
jacsondosreis-jba <jacsonh2.dosreis@gmail.com>
2026-03-03 11:51:26 -0300

    refact: header refatorado

---
+++ meu-novo-app/src/header.jsx
@@ -0,0 +1,20 @@
+
+import './header.css'
+
+function Header(){
+    return(
+       <header>
+        <nav>
+            <h1>TechNova</h1>
+            <ul>
+                <li><a href="#home">Início</a></li>
+                <li><a href="#services">Serviços</a></li>
+                <li><a href="#contact">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    )
+}
+
+export default Header;
