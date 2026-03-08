fe8d82b93107b822dbeb74e9aba3db6c819c85ff
jacsondosreis-jba <jacsonh2.dosreis@gmail.com>
2026-03-08 13:33:51 -0300

    alteração no section-card

--- meu-novo-app/src/components/main.jsx
+++ meu-novo-app/src/components/main.jsx
@@ -12,10 +12,30 @@ function Main(){
             <button id="cta-btn">Saiba Mais</button>
         </section>
 
-        <section id="services" class="cards-section">
+        <section  className="cards-section">
             <h2>Nossas Especialidades</h2>
-            <div></div>
-          {/*   <div id="cards-container" class="cards-grid"></div> */}
+            <div class="cards-grid">
+                <div className='card'>
+                    <h2>UI/UX Design</h2>
+                    <h4>Criação de interfaces intuitivas, acessíveis e focadas na experiência do usuário</h4>
+                </div>
+
+                <div className='card'>
+                    <h2>Infraestrutura AWS</h2>
+                    <h4>Arquitetura, deploy e manutenção de soluções escaláveis na nuvem.</h4>
+                </div>
+
+                <div className='card'>
+                   <h2> IA & Game Dev</h2>
+                    <h4>  Desenvolvimento de jogos integrados com algoritmos de Inteligência Artificial.</h4>
+                </div>
+
+                <div className='card'>
+                   <h2>Engenharia de Dados</h2> 
+                   <h4>Estruturação de dashboards em Power BI e modelagem de dados para tomada de decisão.</h4>
+                </div>
+            </div>
+         
         </section>
     </main>
     
